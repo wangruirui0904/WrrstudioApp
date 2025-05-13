70a65802265830c5e71b412ac979570c56779291
wangruirui0904 <1129442527@qq.com>
2025-05-13 23:15:27 +0800

    git remote set-url origin https://github.com/wangruirui0904/WrrstudioApp.git

---
+++ app/src/main/java/com/example/birthdaytapp/Ratectivity.java
@@ -0,0 +1,49 @@
+package com.example.birthdaytapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Ratectivity extends AppCompatActivity {
+
+    TextView show;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ratectivity);
+        show=findViewById(R.id.rmb_show);
+    }
+    public void click(View btn){
+        //获取输入数据
+        EditText input =findViewById(R.id.rmb);
+        String inpStr=input.getText().toString();
+        try {
+            float rmb = Float.parseFloat(inpStr);
+            float result = 0.0f;
+            //计算
+            if (btn.getId() == R.id.btn_dollar) {
+                result = rmb * 0.1f;
+            } else if (btn.getId() == R.id.btn_euro) {
+                result = rmb * 0.05f;
+            } else if (btn.getId() == R.id.btn_won) {
+
+                result = rmb * 500f;
+            }
+
+            //输出
+            show.setText(String.valueOf(result));
+        }catch(NumberFormatException ex){
+            show.setText("请输入正确数据");
+            Toast.makeText(this, "请输入正确数据", Toast.LENGTH_LONG).show();
+        }
+
+    }
+}
