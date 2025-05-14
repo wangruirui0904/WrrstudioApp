ba9456e487f4e0490648e6060e234e2e1fd19248
wangruirui0904 <1129442527@qq.com>
2025-05-14 21:10:42 +0800

    git remote set-url origin https://github.com/wangruirui0904/WrrstudioApp.git

--- app/src/main/java/com/example/birthdaytapp/ScoreActivity.java
+++ app/src/main/java/com/example/birthdaytapp/ScoreActivity.java
@@ -12,34 +12,52 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class ScoreActivity extends AppCompatActivity {
+    private static final String TAG ="ScoreActivity";
+    private int score1=0;
+    private int score2=0;
 
-
-    TextView score;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_seconf);
+        setContentView(R.layout.activity_score);
+    }
+    public void clicka(View btn){
+        Log.i(TAG, "clicka: 1111");
+        if(btn.getId()==R.id.btn_1){
+            score1+=3;
+        }else if(btn.getId()==R.id.btn_2){
+            score1 +=2;
+        }else{
+            score1 +=1;
+        }
+        show(R.id.score1,score1);
 
-        score = (TextView) findViewById(R.id.score);
+    }
+    public void clickb(View btn) {
+        Log.i(TAG, "clickb:222");
+        if (btn.getId() == R.id.btn_4) {
+            score2 += 3;
+        } else if (btn.getId() == R.id.btn_5) {
+            score2 += 2;
+        } else {
+            score2 += 1;
+        }
+        show(R.id.score2, score2);
+    }
+    private void show(int p,int score){
+        TextView s=findViewById(p);
+        s.setText(String.valueOf(score));
+    }
+    public void reset(View btn) {
+        Log.i(TAG, "reset");
 
+        score1 = 0;
+        score2 = 0;
+        show(R.id.score1, score1);
+        show(R.id.score2, score2);
     }
-    public void btnAdd1(View btn) {
-        showScore(1);
-    }
-    public void btnAdd2(View btn) {
-        showScore(2);
-    }
-    public void btnAdd3(View btn) {
-        showScore(3);
-    }
-    public void btnReset(View btn) {
-        score.setText("0");
-    }
-    private void showScore(int inc) {
-        Log.i("show", "inc="+inc);
-        String oldScore = (String) score.getText();
-        int newScore = Integer.parseInt(oldScore) + inc;
-        score.setText(""+newScore);
-    }
+
+
+
 }
