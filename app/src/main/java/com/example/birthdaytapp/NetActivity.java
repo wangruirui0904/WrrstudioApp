a4982b850f820152f9656a95f6a8fb49614950b0
wangruirui0904 <1129442527@qq.com>
2025-05-15 10:52:46 +0800

    git remote set-url origin https://github.com/wangruirui0904/WrrstudioApp.git

--- app/src/main/java/com/example/birthdaytapp/NetActivity.java
+++ app/src/main/java/com/example/birthdaytapp/NetActivity.java
@@ -13,6 +13,14 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class NetActivity extends AppCompatActivity implements Runnable {
 
     private static final String TAG = "Net";
@@ -27,9 +35,9 @@ public class NetActivity extends AppCompatActivity implements Runnable {
             @Override
             public void handleMessage(@NonNull Message msg) {
                 //处理返回
-                if(msg.what==5){
+                if (msg.what == 5) {
                     String str = (String) msg.obj;
-                    Log.i(TAG, "handleMessage: str="+str);
+                    Log.i(TAG, "handleMessage: str=" + str);
 
                 }
                 super.handleMessage(msg);
@@ -42,12 +50,44 @@ public class NetActivity extends AppCompatActivity implements Runnable {
         t.start();
     }
 
-        @Override
-        public void run () {
-            Log.i(TAG, "run: 子线程（）.....");
+    @Override
+    public void run() {
+        Log.i(TAG, "run: 子线程（）.....");
 
-            //发送消息
-            Message msg = handler.obtainMessage(5,"Hello from thread");
-            handler.sendMessage(msg);
+        //获网络数据
+        URL url = null;
+        try {
+            url = new URL("https://www.swufe.edu.cn/info/1067/37271.htm");
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
+            InputStream in = http.getInputStream();
+
+            String html = inputStream2String(in);
+            Log.i(TAG, "run: html=" + html);
+
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
+
+        //发送消息
+        Message msg = handler.obtainMessage(5, "Hello from thread");
+        handler.sendMessage(msg);
     }
+
+    private String inputStream2String(InputStream inputStream) throws IOException {
+        final int bufferSize = 1024;
+        final char[] buffer = new char[bufferSize];
+        final StringBuilder out = new StringBuilder();
+        Reader in = new InputStreamReader(inputStream, "utf-8");
+        while (true) {
+            int rsz = in.read(buffer, 0, buffer.length);
+            if (rsz < 0)
+                break;
+            out.append(buffer, 0, rsz);
+        }
+        return out.toString();
+
+
+    }
+}
