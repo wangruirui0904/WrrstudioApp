2b245460faf601117babdc2e3224332218094823
wangruirui0904 <1129442527@qq.com>
2025-05-28 00:25:46 +0800

    git remote set-url origin https://github.com/wangruirui0904/WrrstudioApp.git

--- app/src/main/java/com/example/birthdaytapp/Final_1Activity.java
+++ app/src/main/java/com/example/birthdaytapp/Final_1Activity.java
@@ -18,15 +18,10 @@ public class Final_1Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_final1);
-
-        // 绑定按钮
-        Button btn_Enter = findViewById(R.id.btn_enter);
-
-        // 设置点击事件
-        btn_Enter.setOnClickListener(new View.OnClickListener() {
+        Button btn_enter = findViewById(R.id.btn_enter);
+        btn_enter.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View btn) {
-                // 创建 Intent 跳转到 Final2Activity
                 Intent intent = new Intent(Final_1Activity.this, Final_2Activity.class);
                 startActivity(intent);
             }
--- app/src/main/java/com/example/birthdaytapp/Final_2Activity.java
+++ app/src/main/java/com/example/birthdaytapp/Final_2Activity.java
@@ -1,6 +1,8 @@
 package com.example.birthdaytapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,13 @@ public class Final_2Activity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_final2);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        Button button2 = findViewById(R.id.button_2);
+        button2.setOnClickListener(v -> {
+            // 创建 Intent 跳转到 Final_2Activity
+            Intent intent = new Intent(Final_2Activity.this, Final_2_1Activity.class);
+            startActivity(intent);
         });
     }
 }
+
